ef35d83d543d050d3345359d037843991bfdecd7
Keiichi Lindley <klindley@simplehealth.com>
2021-10-18 09:43:48 -0600

    Remove lib and use all in one file instead

--- index.js
+++ index.js
@@ -1,7 +1,63 @@
+const fs = require("fs");
+const ora = require("ora");
 const npm = require("npm");
 const path = require("path");
 const inquirer = require("inquirer");
-const NPScripts = require("./lib/NPScripts");
+const { projectInstallSync } = require("pkg-install");
+
+class NPScripts {
+  
+  constructor() {
+    if (!fs.existsSync(path.resolve(process.cwd(), "package.json"))) {
+      console.error(`No package.json file could be found at ${process.cwd()}`);
+      process.exit(0);
+    } else {
+      this.package = require(path.resolve(process.cwd(), "package.json"));
+    }
+    if (!this.package || !this.package.hasOwnProperty("scripts")) {
+      console.error(`[${this.package.name.toUpperCase()}] does not have any scripts`);
+      process.exit(0);
+    }
+    let { name, description, scripts} = this.package;
+    return {
+      name,
+      scripts,
+      description,
+      package: this.package,
+      dependencies: this.dependencies()
+    }
+  }
+
+  dependencies () {
+    let { dependencies = {}, devDependencies = {} } = this.package;
+    let all = Object.assign({}, dependencies, devDependencies);
+    let packages = Object.keys(all);
+    let installed = fs.existsSync(
+      path.resolve(process.cwd(), "node_modules")
+    );
+    let install = async (shouldInstall) => {
+      return new Promise(async (res, rej) => {
+        let spinner = ora();
+        if (!installed) {
+          spinner.start("Installing Dependencies");
+          let installDeps = await projectInstallSync({
+            verbose: true,
+            dev: true,
+            prefer: "npm",
+            stdio: "inherit",
+          })
+        }
+        res(spinner.succeed());
+      })
+    }
+    return {
+      all,
+      install,
+      packages,
+      installed,
+    }
+  }
+}
 
 let npscripts = new NPScripts();
 
